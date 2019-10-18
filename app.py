62c6bdeb752b3a4ad9b59e4ce6c35fc281f8e588
Hyeontaek Oh <tax941116@gmail.com>
2019-10-19 00:46:06 +0900

    - make map POST function

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'this is home'
-
-@app.route('/map')
-def map():
     return render_template('map.html')
 
+
+@app.route('/map', methods=['POST'])
+def saving():
+    user_receive = request.form['user_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+    lat_receive = request.form['lat_give']
+    lng_receive = request.form['lng_give']
+    name_receive = request.form['name_give']
+
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
+
+@app.route('/map', methods=['GET'])
+def listing():
+    name_receive = request.args.get('name_give')
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5002, debug=True)
+    app.run('localhost', port=5000, debug=True)
