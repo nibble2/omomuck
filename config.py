df207b16d7671ad66ebf239661d437c36933d383
nibble2 <nibblekk@gmail.com>
2019-11-01 19:28:31 +0900

    config file

---
+++ config.py
@@ -0,0 +1,13 @@
+#config.py
+
+API_KEY = {
+    'kakao_map_api': 'fdc9dfae2cba344c2ba1f75e9e580226'
+}
+
+DB_CONFIG = {
+    'stores_host': 'empo.cepuule7ur6a.ap-northeast-2.rds.amazonaws.com',
+    'stores_port': 3306,
+    'stores_user': 'admin',
+    'stores_passwd': 'dleksk**',
+    'stores_db': 'DB',
+}
